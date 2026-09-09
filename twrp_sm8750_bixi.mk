bfce9423f4dc45ec6d6d84e06494439c65d83c7c
daixu0502 <daixu0502@outlook.com>
2026-09-09 21:13:56 +0800

    first commit

---
+++ twrp_sm8750_bixi.mk
@@ -0,0 +1,20 @@
+##
+# Copyright (C) 2025 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/xiaomi/bixi
+
+# Inherit from device.mk configuration
+$(call inherit-product, $(DEVICE_PATH)/device.mk)
+
+## Device identifier
+PRODUCT_DEVICE := xiaomi_bixi
+PRODUCT_NAME := twrp_xiaomi_bixi
+PRODUCT_BRAND := xiaomi
+PRODUCT_MODEL := 2505APX7BC
+PRODUCT_MANUFACTURER := xiaomi
+
+# Theme
+TW_STATUS_ICONS_ALIGN := center
